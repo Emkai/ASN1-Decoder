d80e3c8779420fcfd5cf6d2170a89bfd733f6267
AdamBohman <adambohman@live.com>
2015-11-09 16:58:58 +0100

    Interface
    Started to build the GUI

---
+++ src/gui/DecoderGUI.java
@@ -0,0 +1,82 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+import java.awt.event.ActionListener;
+import java.awt.List;
+import java.awt.Choice;
+
+public class DecoderGUI {
+
+	private JFrame frame;
+	private JTextField txtEnterFilePath;
+
+	/**
+	 * @wbp.nonvisual location=188,179
+	 */
+	
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DecoderGUI window = new DecoderGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public DecoderGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		
+		frame = new JFrame();
+		frame.setBounds(100, 100, 430, 120);
+		frame.setTitle("ASN1-Decoder");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	
+	
+		
+		JButton btnBrowse = new JButton("Browse...");
+		btnBrowse.setBounds(309, 30, 89, 23);
+		frame.getContentPane().add(btnBrowse);
+	
+	
+	
+		
+		txtEnterFilePath = new JTextField();
+		txtEnterFilePath.setText("Enter file path...");
+		txtEnterFilePath.setBounds(10, 30, 289, 23);
+		frame.getContentPane().add(txtEnterFilePath);
+		txtEnterFilePath.setColumns(10);
+	}
+
+
+		
+	
+	}
+

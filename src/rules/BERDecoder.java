cd6fde0e251c790ab0525c12470b10d413f82dab
matkal-2 <matkal-2@student.ltu.se>
2015-11-09 16:59:49 +0100

    Fixed more with BERDecoding
    Fixed more with BERDecoding, didnt fix the readLength. Still cant handle
    long length octets or indefinate length octates.

--- src/rules/BERDecoder.java
+++ src/rules/BERDecoder.java
@@ -4,24 +4,47 @@ import java.util.ArrayList;
 
 public class BERDecoder extends Decoder{
 	private byte[] bytes;
+	private int byteCurrently;
 
 	public BERDecoder(byte[] bytes) {
 		this.bytes = bytes;
-		ArrayList<String> tag = new ArrayList<String>();
+		this.byteCurrently = 0;
 		
-		tag = readTag(0);
-		System.out.println(tag.get(0));
-		System.out.println(tag.get(1));
-		System.out.println(tag.get(2));
-		
-		int length = readLength(1);
-		System.out.println(length);
+		decodeTag();
 		
 	}
 	
-	private ArrayList<String> readTag(int byteIndex){
+	private void decodeTag(){
 		ArrayList<String> tag = new ArrayList<String>();
-		switch ( this.bytes[byteIndex] >> 6 ) {
+		ArrayList<Byte> dataBytes = new ArrayList<Byte>();
+		System.out.print("Byte: " + this.byteCurrently +", "+ this.bytes[this.byteCurrently]+", ");
+		System.out.println(this.bytes[this.byteCurrently+1]);
+		tag = readTag();
+		System.out.print("Class: " + tag.get(0));
+		System.out.print(", P/C: " + tag.get(1));
+		System.out.print(", Tag: " + tag.get(2));
+		int length = readLength();
+		System.out.print(", Lenght: " + length);
+		System.out.println("");
+		
+		if(tag.get(1) == "Primitive"){
+			for (int i = this.byteCurrently; i> (this.byteCurrently+length); ){
+				dataBytes.add(bytes[i]);
+			}
+			
+		}
+		else{
+			int endByte = this.byteCurrently+length;
+			while ( this.byteCurrently <= endByte){
+				this.byteCurrently++;
+				decodeTag();
+			}
+		}
+	}
+	
+	private ArrayList<String> readTag(){
+		ArrayList<String> tag = new ArrayList<String>();
+		switch ( this.bytes[this.byteCurrently] >> 6 ) {
 			case 0:
 				tag.add("Universal");
 				break;
@@ -36,32 +59,63 @@ public class BERDecoder extends Decoder{
 				break;
 		}
 		
-		if ( ((this.bytes[byteIndex] >> 5) & 1 ) == 1 ){
+		if ( ((this.bytes[this.byteCurrently] >> 5) & 1 ) == 1 ){
 			tag.add("Constructed");
 		}
 		else{
 			tag.add("Primitive");
 		}
 		
-		if (  (this.bytes[byteIndex]&0x1f ) > 30 ){
-			tag.add("Long Size type");
+		if (  (this.bytes[this.byteCurrently]&0x1f ) > 30 ){
+			tag.add(readLongType());
 		}
 		else{
-			String s = "" + (this.bytes[byteIndex]&0x1f);
+			String s = "" + (this.bytes[this.byteCurrently]&0x1f);
 			tag.add(s);
+			this.byteCurrently++;
 		}
 		
 		return tag;
 	}
 	
-	private int readLength(int byteIndex){
-		if ( this.bytes[byteIndex] == 0x80 ){
-			System.out.println("Long length");
+	private String readLongType(){
+		this.byteCurrently++;
+		String type = "" + (this.bytes[this.byteCurrently]&0x7F);
+		if((this.bytes[this.byteCurrently]>>7 & 1) == 1){
+			type += readLongType();
+		}
+		return "" + (this.bytes[this.byteCurrently]&0x07F);
+	}
+	
+	
+	
+	private int readLength(){
+		if ( (this.bytes[this.byteCurrently] >> 7 & 1) == 1 ){
+			if((this.bytes[this.byteCurrently]&0x07F) != 0){
+				int lengthOctets = this.bytes[this.byteCurrently]&0x07F;
+				System.out.println(" Lengthy: "+ lengthOctets);
+				String length = "";
+				for(int i = 1; i < lengthOctets+1; i++){
+					length += this.bytes[this.byteCurrently + i]&0x07F;
+				}
+				return Integer.parseInt(length);
+			}
 			return 0;
+			
 		}
 		else{
-			return (int)this.bytes[byteIndex]&0x0FF;
+			this.byteCurrently++;
+			return (int)this.bytes[this.byteCurrently]&0x0FF;
 		}
+	}
+	
+	private String readIndefinateLength(){
+		this.byteCurrently++;
+		String type = "" + (this.bytes[this.byteCurrently]&0x7F);
+		if((this.bytes[this.byteCurrently]>>7 & 1) == 1){
+			type += readLongType();
+		}
+		return "" + (this.bytes[this.byteCurrently]&0x7F);
 	}
 
 }
